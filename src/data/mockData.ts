0c2625b5f7c228fce7b8a879d458c5aa98bdf900
Aziez Houssam Eddine <70609463+houssamaziez@users.noreply.github.com>
2025-08-28 16:44:10 +0100

    Updated mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -272,7 +272,7 @@ export const testimonials = [
   },
   {
     id: "3",
-    name: "houssam eddine",
+    name: "Malak",
     role: "Content Creator",
     avatar: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=150",
     rating: 5,
